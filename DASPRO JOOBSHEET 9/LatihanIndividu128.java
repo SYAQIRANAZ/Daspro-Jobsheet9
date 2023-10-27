ce8d5c8ce035276c1a48e9df1d0b1401f81e9959
SYAQIRANAZ <144090657+SYAQIRANAZ@users.noreply.github.com>
2023-10-27 09:58:33 +0700

    Add files via upload

---
+++ DASPRO JOOBSHEET 9/LatihanIndividu128.java
@@ -0,0 +1,11 @@
+
+public class LatihanIndividu128 {
+
+    public static void main(String[] args) {
+        int angka[] = new int[60];
+
+        for (int i = 0; i < angka.length; i++) {
+            System.out.println( i );
+        }
+    }
+}
---
+++ DASPRO JOOBSHEET 9/LatihanIndividu228.java
@@ -0,0 +1,13 @@
+
+public class LatihanIndividu228 {
+    public static void main(String[] args) {
+        int angka[] = new int[10];
+
+        for (int i = 9;  i < angka.length; i--) {
+            angka[i] = i+1;
+           System.out.println(i);
+    
+        }
+        
+    }
+}
