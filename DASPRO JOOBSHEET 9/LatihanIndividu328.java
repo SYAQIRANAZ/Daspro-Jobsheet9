f8f119bee55e3376296842af48fd997f6af9d815
SYAQIRANAZ <144090657+SYAQIRANAZ@users.noreply.github.com>
2023-10-27 10:51:23 +0700

    Add files via upload

---
+++ DASPRO JOOBSHEET 9/LatihanIndividu328.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class LatihanIndividu3 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int ang[];
+        ang = new int[12];
+        String[] bulan = {"Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"};
+
+        for (int i = 0; i< bulan.length; i++){
+              System.out.println("Bulan ke "+i+ " adalah bulan " +bulan[i]);
+
+         }
+          
+        }
+        
+    }
